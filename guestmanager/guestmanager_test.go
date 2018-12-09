477816cb7912d5e4520fb0f45e13cba42071cf18
Joshua Vécsei <git@vecsei.me>
2018-12-09 13:58:35 +0100

    testcase naming fixed

---
+++ guestmanager/guestmanager_test.go
@@ -0,0 +1,155 @@
+package guestmanager
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/JVecsei/fritz-guest/session"
+)
+
+func TestGuestManager_TurnOn(t *testing.T) {
+	getConfig := false
+	setConfig := false
+	d := DataResponse{
+		Data: Data{
+			GuestAccess: GuestAccess{
+				Psk:  "private",
+				SSID: "public",
+			},
+		},
+	}
+	res, _ := json.Marshal(d)
+
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		if req.URL.String() == "/data.lua" && req.Method == "POST" && req.FormValue("xhrId") == "all" {
+			rw.Write(res)
+			getConfig = true
+		} else if req.URL.String() == "/data.lua" && req.Method == "POST" && req.FormValue("psk") != "" {
+			if req.FormValue("psk") != "private" || req.FormValue("ssid") != "public" {
+				t.Errorf("unexpected values '%s' should be 'private' and '%s' should be 'public'", req.FormValue("psk"), req.FormValue("ssid"))
+			}
+			rw.Write(res)
+			setConfig = true
+		}
+	}))
+
+	s := &session.Session{
+		BlockTime: 0,
+		Challenge: "321",
+		SID:       "123",
+		URL:       server.URL,
+		Username:  "",
+	}
+	g, err := NewGuestManager(s)
+
+	if err != nil {
+		t.Errorf("err should be nil but was %v", err)
+	}
+	err = g.TurnOn()
+
+	if err != nil {
+		t.Errorf("err should be nil but was %v", err)
+	}
+
+	if !getConfig || !setConfig {
+		t.Errorf("%v should be true and %v should be true", getConfig, setConfig)
+	}
+
+}
+
+func TestGuestManager_TurnOnWithPsk(t *testing.T) {
+	getConfig := false
+	setConfig := false
+	d := DataResponse{
+		Data: Data{
+			GuestAccess: GuestAccess{
+				Psk:  "private",
+				SSID: "public",
+			},
+		},
+	}
+	res, _ := json.Marshal(d)
+
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		if req.URL.String() == "/data.lua" && req.Method == "POST" && req.FormValue("xhrId") == "all" {
+			rw.Write(res)
+			getConfig = true
+		} else if req.URL.String() == "/data.lua" && req.Method == "POST" && req.FormValue("psk") != "" {
+			if req.FormValue("psk") != "privateNew" || req.FormValue("ssid") != "public" {
+				t.Errorf("unexpected values '%s' should be 'private' and '%s' should be 'public'", req.FormValue("psk"), req.FormValue("ssid"))
+			}
+			rw.Write(res)
+			setConfig = true
+		}
+	}))
+
+	s := &session.Session{
+		BlockTime: 0,
+		Challenge: "321",
+		SID:       "123",
+		URL:       server.URL,
+		Username:  "",
+	}
+	g, err := NewGuestManager(s)
+
+	if err != nil {
+		t.Errorf("err should be nil but was %v", err)
+	}
+	err = g.TurnOnWithPsk("privateNew")
+
+	if err != nil {
+		t.Errorf("err should be nil but was %v", err)
+	}
+
+	if !getConfig || !setConfig {
+		t.Errorf("%v should be true and %v should be true", getConfig, setConfig)
+	}
+
+}
+
+func TestGuestManager_TurnOff(t *testing.T) {
+	setConfig := false
+	d := DataResponse{
+		Data: Data{
+			GuestAccess: GuestAccess{
+				Psk:       "private",
+				SSID:      "public",
+				IsEnabled: "false",
+			},
+		},
+	}
+	res, _ := json.Marshal(d)
+
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		if req.URL.String() == "/data.lua" && req.Method == "POST" && req.FormValue("isEnabled") == "0" {
+			rw.Write(res)
+			setConfig = true
+		} else {
+			t.Errorf("did not receive isEnabled = 0 request")
+		}
+	}))
+
+	s := &session.Session{
+		BlockTime: 0,
+		Challenge: "321",
+		SID:       "123",
+		URL:       server.URL,
+		Username:  "",
+	}
+	g, err := NewGuestManager(s)
+
+	if err != nil {
+		t.Errorf("err should be nil but was %v", err)
+	}
+	err = g.TurnOff()
+
+	if err != nil {
+		t.Errorf("err should be nil but was %v", err)
+	}
+
+	if !setConfig {
+		t.Errorf("%v should be true", setConfig)
+	}
+}
